8d6aa091a1cd9e59d92f0c713399df43d77474d6
Sk Shahnawaz-ul Haque <79637019+Shahnawaz-QI@users.noreply.github.com>
2021-09-06 20:06:14 +0530

    feat: Added input data validation. (#1)

--- web-api/dto/data.go
+++ web-api/dto/data.go
@@ -1,5 +1,20 @@
 package dto
 
+import (
+	"github.com/go-ozzo/ozzo-validation/is"
+	validator "github.com/go-ozzo/ozzo-validation/v4"
+)
+
 type Input struct {
-	Url string `json:"url" validate:"required"`
+	Url string `json:"url"`
+}
+
+func (input Input) Validate() error {
+	return validator.ValidateStruct(&input,
+		validator.Field(&input.Url, validator.Required.Error("'url' is required.")),
+		validator.Field(&input.Url, is.URL.Error("Provide proper URL as value of 'url'")))
+}
+
+func Validate(queryString string) error {
+	return validator.Validate(queryString, validator.Required.Error("Provide value of query string"))
 }
